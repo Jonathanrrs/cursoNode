35032a75b14e180ac805f80d05fb9a4cc6958e9d
jonathan <yoni_12rrs@hotmail.com>
2021-10-26 22:45:36 -0500

    Funciones de Flecha

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,16 @@
+// function sumar(a,b) {
+//     return a + b ;
+// }
+
+// const sumar = (a,b = 7) => {
+//     return a+ b;
+// }
+const sumar = (a,b) => a + b;
+
+const saludar = () => 'Hola mundo';
+
+const multiplicar = num => num * 10; 
+
+console.log(sumar(7,10));
+console.log(saludar());
+console.log(multiplicar(7));
