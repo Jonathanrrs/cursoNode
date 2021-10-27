fc80c807a4f55b66527ee075e4b6431d76548fba
jonathan <yoni_12rrs@hotmail.com>
2021-10-27 15:17:02 -0500

    Requerir paquetes

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,21 @@
+const fs = require('fs');
+
 console.clear();
 console.log('================');
 console.log('   Tabla del 5');
 console.log('================');
-const num = 5;
+
+const num = 7;
+let salida = '';
 for (let index = 1; index <= 10; index++) {
     
     const multiplicar = num*index;
-    console.log(`${num} x ${index} = ${multiplicar}`);
-    
+    salida += `${num} x ${index} = ${multiplicar}\n`; /* \n salto de linea */
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${num}.txt`, salida, (err) => {
+   if(err) throw err;
+   console.log(`Tabla-${num}.txt creado`); 
+})
