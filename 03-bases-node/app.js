76b60800caf33bd5de50d557eeefbe0f6dc4ba07
jonathan <yoni_12rrs@hotmail.com>
2021-10-27 15:03:20 -0500

    bases de node

---
+++ 03-bases-node/app.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log('================');
+console.log('   Tabla del 5');
+console.log('================');
+const num = 5;
+for (let index = 1; index <= 10; index++) {
+    
+    const multiplicar = num*index;
+    console.log(`${num} x ${index} = ${multiplicar}`);
+    
+}
