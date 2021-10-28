43a0a4f3cb10a799a64c658c863122fd51a6c68b
jonathan <yoni_12rrs@hotmail.com>
2021-10-28 15:32:32 -0500

    Confirmar y borrar tarea

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -2,11 +2,12 @@ require('colors');
 
 
 const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
-const { 
+const {
     inquirerMenu,
-     pausaMenu, 
-     leerInput,
-     listadoTareasBorrar
+    pausaMenu,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar
 } = require('./helpers/inquirer');
 // const Tarea = require('./models/tarea');
 const Tareas = require('./models/tareas');
@@ -22,38 +23,44 @@ const main = async () => {
     const tareas = new Tareas();
     const tareasDB = leerDB();
 
-    if(tareasDB) {
+    if (tareasDB) {
         /* establecer las tareas */
         tareas.cargarTareasFromArray(tareasDB);
     }
-    
-    
+
+
     do {
         /* imprimir menú */
         opt = await inquirerMenu();
-        
+
         switch (opt) {
             case '1':
                 const desc = await leerInput('Descripción:');
                 tareas.crearTarea(desc);
-            break;
+                break;
             case '2':
                 tareas.listadoCompleto();
                 // console.log(tareas.listadoArr);
-            break
+                break
             case '3':
                 tareas.listarPendientesCompletadas(true);
                 break
-                case '4':
+            case '4':
                 tareas.listarPendientesCompletadas(false);
                 // tareas.listarPendientes();
-            break
+                break
             case '6':
                 const id = await listadoTareasBorrar(tareas.listadoArr);
-                console.log({id});
-            break
+                if(id !== '0') {
+                    const ok = await confirmar('¿Estás seguro?');
+                    if(ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada');
+                    }
+                }
+                break
         }
-       
+
         guardarDB(tareas.listadoArr);
 
         await pausaMenu();
