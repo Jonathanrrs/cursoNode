2c547221b485bc6e315bad07e9b14b1ad0780e63
jonathan <yoni_12rrs@hotmail.com>
2021-10-28 15:53:34 -0500

    Marcar como completadas o pendientes las tareas

--- 04-tareas-hacer/models/tareas.js
+++ 04-tareas-hacer/models/tareas.js
@@ -61,7 +61,7 @@ class Tareas {
             if(completadas) {
                 if(completadoEn) {
                     contador += 1;
-                    console.log(`${(contador + '.').green} ${desc} :: ${completadoEn}`);
+                    console.log(`${(contador + '.').green} ${desc} :: ${completadoEn.green}`);
                 }
             } else {
                 if(!completadoEn) {
@@ -70,6 +70,21 @@ class Tareas {
                 }
             }
         });
+    }
+
+    toggleCompletadas(ids = []) {
+        ids.forEach(id => {
+            const tarea = this._listado[id];
+            if(!tarea.completadoEn) {
+                tarea.completadoEn = new Date().toISOString()
+            }
+        });
+
+        this.listadoArr.forEach(tarea => {
+            if(!ids.includes(tarea.id)) {
+               this._listado[tarea.id].completadoEn = null;
+            }
+        })
     }
     
 }
