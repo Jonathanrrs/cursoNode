ae9bffd96bee7dec6c34b7b9d44e38acdd8119c6
jonathan <yoni_12rrs@hotmail.com>
2021-10-31 13:33:46 -0600

    Menu de la aplicación

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,9 +1,24 @@
-const { leerInput } = require("./helpers/inquirer")
+const { inquirerMenu, pausaMenu } = require("./helpers/inquirer")
 
 
 const main = async() => {
-    const texto = await leerInput('Hola: ');
-    console.log(texto);
+    
+    let opt;
+
+    do {
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                console.log('opcion1');
+                break;
+            case 2: 
+                console.log('opcion2');
+                break;
+            
+        }
+        await pausaMenu();
+    } while (opt !== 0);
 }
 
 main();
