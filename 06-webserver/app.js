8a93a68afea34acff18c65514e60c782596c34e7
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 13:51:00 -0600

    Servir un sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -13,9 +13,17 @@ app.get('/hola-mundo', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/generic', (req,res) => {
+    res.sendFile(__dirname+'/public/generic.html')
+});
+app.get('/elements', (req,res) => {
+    res.sendFile(__dirname+'/public/elements.html')
+});
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname+'/public/404.html');
-})
+});
+
 app.listen(port, () => {
     console.log('Corriendo en el puerto', port);
 })
