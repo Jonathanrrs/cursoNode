1332defdf2ceff189731b9d4ef85e910aebc5207
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 13:17:17 -0600

    Introducción a EXPRESS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,22 +1,18 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+ const port = 8080;
+ 
+app.get('/', (req, res) => {
+  res.send('Home page');
+});
 
-http.createServer((req,res) => {
-    
-    // res.writeHead(200, {'Content-Type': 'application/json'});
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-    res.writeHead(200, {'Content-Type': 'application/csv'});
+app.get('/hola-mundo', (req, res) => {
+  res.send('Hello World');
+});
 
-    // const persona = {
-    //     id: 1,
-    //     nombr: 'Jonathan'
-    // }
-
-    // res.write(JSON.stringify(persona));
-    res.write('id, nombre\n');
-    res.write('1, Jonathan\n');
-    res.write('2, Ramiro\n');
-    res.end();
+app.get('*', (req, res) => {
+  res.send('404 | Page not found');
 })
-.listen(8080);
-
-console.log('Escuchando en el puerto', 8080);
+app.listen(port, () => {
+    console.log('Corriendo en el puerto', port);
+})
