9c0f66fc5fddf60f4194ca8f0f44258b613ec184
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 13:03:59 -0600

    Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
 http.createServer((req,res) => {
+    
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {'Content-Type': 'application/csv'});
 
-    res.write('Hola mundo');
+    // const persona = {
+    //     id: 1,
+    //     nombr: 'Jonathan'
+    // }
+
+    // res.write(JSON.stringify(persona));
+    res.write('id, nombre\n');
+    res.write('1, Jonathan\n');
+    res.write('2, Ramiro\n');
     res.end();
 })
 .listen(8080);
