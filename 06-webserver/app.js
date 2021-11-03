00d86d0bd2b2f55e0370c9db09b8ba6b910e2988
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 13:31:23 -0600

    Servir contenido estático

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
  const port = 8080;
- 
-app.get('/', (req, res) => {
-  res.send('Home page');
-});
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.send('Home page');
+// });
 
 app.get('/hola-mundo', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('*', (req, res) => {
-  res.send('404 | Page not found');
+  res.sendFile(__dirname+'/public/404.html');
 })
 app.listen(port, () => {
     console.log('Corriendo en el puerto', port);
