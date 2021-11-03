0ff492ef3a092918ce061f85c4f0868799ec76c5
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 01:00:34 -0600

    Leer archivo y capitalizar

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -46,7 +46,7 @@ const main = async() => {
                 console.log('Como está el clima:',clima.desc.green);
                 break;
             case 2: 
-                busquedas.historial.forEach((lugar, i) => {
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
                     const idx = `${i+1}.`.green;
                     console.log(`${idx} ${lugar}`);
                 })
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -8,6 +8,15 @@ class Busquedas {
 
     constructor() {
         //Todo: leer Db si existe
+        this.leerDB();
+    }
+
+    get historialCapitalizado() {
+       return this.historial.map(lugar => {
+           let palabras = lugar.split(' ');
+           palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+           return palabras.join(' ')
+       })
     }
 
     get paramsMapBox() {
@@ -78,7 +87,8 @@ class Busquedas {
         if(this.historial.includes(lugar.toLocaleLowerCase())) {
             return;
         }
-        this.historial.unshift(lugar);
+        this.historial = this.historial.splice(0,4)
+        this.historial.unshift(lugar.toLocaleLowerCase());
         
         // Grabar en DB
         this.guardarDB();
@@ -91,9 +101,15 @@ class Busquedas {
 
         fs.writeFileSync(this.dbPath, JSON.stringify(payload));
     }
-    // leerDB() {
-
-    // }
+    leerDB() {
+        if(fs.existsSync(this.dbPath)) {
+            const info = fs.readFileSync(this.dbPath, 'utf-8');
+            const data = JSON.parse(info);
+            this.historial = data.historial;
+        } else {
+            return;
+        }
+    }
 }
 
 module.exports = Busquedas;
