153e2534e0f8eb2208fe769db9cea92c68b43b43
jonathan <yoni_12rrs@hotmail.com>
2021-11-03 14:05:34 -0600

    Handlebars

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
  const port = 8080;
 
+app.set('view engine', 'hbs');
+
+
 // Servir contenido estático
 app.use(express.static('public'));
 
@@ -9,8 +12,8 @@ app.use(express.static('public'));
 //   res.send('Home page');
 // });
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hello World');
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
 app.get('/generic', (req,res) => {
