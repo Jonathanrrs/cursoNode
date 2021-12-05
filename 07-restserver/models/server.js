1b91b2da093fa39121781750b798622270900135
jonathan <yoni_12rrs@hotmail.com>
2021-12-05 15:45:00 -0600

    Peticiones HTTP - GET - PUT - POST - DELETE

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -18,8 +18,33 @@ class Server {
     }
 
     routes() {
+
         this.app.get('/api', (req, res) => {
-            res.send('Hello World');
+            res.json({
+                ok: true,
+                msg: 'get API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'delete API'
+            });
         });
 
     }
