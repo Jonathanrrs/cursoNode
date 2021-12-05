a7bd2da16cfb8ea365be66d8c3ffcaa0ddd7802f
jonathan <yoni_12rrs@hotmail.com>
2021-12-05 16:12:37 -0600

    Separar las rutas y el controlador de la clase

---
+++ 07-restserver/controllers/usuarios.js
@@ -0,0 +1,45 @@
+const {response} = require('express');
+
+const usuariosGet = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        msg: 'get API - controlador'
+    });
+};
+
+const usuariosPost = (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'post API - controlador'
+    });
+};
+
+const usuariosPut = (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'put API - controlador'
+    });
+};
+
+const usuariosPatch = (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'patch API - controlador'
+    });
+};
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'delete API- controlador'
+    });
+};
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -6,7 +6,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-
+        this.usuariosPath = '/api/usuarios';
         /* Middlewares */
         this.middlewares();
         /* Rutas de mi aplicación */
@@ -22,33 +22,7 @@ class Server {
 
     routes() {
 
-        this.app.get('/api', (req, res) => {
-            res.json({
-                ok: true,
-                msg: 'get API'
-            });
-        });
-
-        this.app.put('/api', (req, res) => {
-            res.json({
-                ok: true,
-                msg: 'put API'
-            });
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                ok: true,
-                msg: 'post API'
-            });
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                ok: true,
-                msg: 'delete API'
-            });
-        });
+       this.app.use(this.usuariosPath, require('../routes/usuarios'));
 
     }
 
