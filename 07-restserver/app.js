e47c7cffcbc9bb28cba3a0fdb0a54bd40d233ef2
jonathan <yoni_12rrs@hotmail.com>
2021-12-05 15:32:50 -0600

    Express basado en clases

--- 07-restserver/app.js
+++ 07-restserver/app.js
@@ -1,11 +1,6 @@
+
 require('dotenv').config();
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
- 
-app.listen(process.env.PORT, () => {
-    console.log('server running', process.env.PORT);
-});
+const Server = require('./models/server');
+
+ const server = new Server();
+ server.listen();
